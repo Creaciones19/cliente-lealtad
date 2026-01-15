137d5492b96324468fe8d8133af0ab7772dd0d2b
Dynah1970 <sinergiadinahgomez19@gmail.com>
2026-01-15 00:28:16 -0600

    boton app

--- sw.js
+++ sw.js
@@ -1,2 +1,25 @@
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => self.clients.claim());
+const CACHE = "buonapp-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
